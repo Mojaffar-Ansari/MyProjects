d042914c8f204c13b9c76d6a2d74ca294ca39f63
Mojaffar-Ansari <60110312+Mojaffar-Ansari@users.noreply.github.com>
2020-02-08 00:25:48 +0530

    Create MathOperations.py

---
+++ MathOperations.py
@@ -0,0 +1,14 @@
+import math
+a=int(input("Enetr first number:"))
+b=int(input("Enter second number:"))
+print("Sum:",a+b)
+print("Sub:",a-b)
+print("Mul:",a*b)
+print("Div:",a//b) #Gives int value
+x=math.sqrt(a+b)
+print(x)
+print("Flour value:",math.floor(x))
+print("Ceil value:",math.ceil(x))
+print("a to the power b:",math.pow(a,b))
+print("pi value:",math.pi)
+print("e value:",math.e)
